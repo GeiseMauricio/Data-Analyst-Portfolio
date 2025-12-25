c701078655e18d844632252f557d9c399dda7ab5
Geise Maurício <geise.mauricio@gmail.com>
2025-12-25 15:04:23 -0400

    Delete etl directory

--- etl/load.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd # type: ignore
-
-def save_csv(df: pd.DataFrame, path: str):
-    """Salva o dataset limpo."""
-    df.to_csv(path, index=False)
-    print(f"💾 Dados salvos em: {path}")
